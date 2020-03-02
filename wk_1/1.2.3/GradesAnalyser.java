6f0ea2d7ebbbe5f1ef5117ab7e16654cef6f3d2d
Ryan Cetinski <ryancetinskii@gmail.com>
2020-03-03 06:54:51 +1100

    Add answer to task 1.2.3 Q2

---
+++ wk_1/1.2.3/GradesAnalyser.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class GradesAnalyser {
+
+    public GradesAnalyser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the mark for student 1: ");
+        int mark1 = scanner.nextInt();
+
+        System.out.print("Enter the mark for student 2: ");
+        int mark2 = scanner.nextInt();
+
+        System.out.print("Enter the mark for student 3: ");
+        int mark3 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Average grade: %f%nHighest mark: %d%nLowest mark: %d%n",
+                (double) (mark1 + mark2 + mark3) / 3, Math.max(mark1, Math.max(mark2, mark3)),
+                Math.min(mark1, Math.min(mark2, mark3)));
+    }
+
+    public static void main(String[] args) {
+        GradesAnalyser gradesAnalyser = new GradesAnalyser();
+    }
+}
